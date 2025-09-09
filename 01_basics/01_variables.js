dfcfc108b4eadb6cd55c935af1bc2cc52638f2cf
Prathmesh Tiwari <122382508+prathmeshtiwari22@users.noreply.github.com>
2025-09-09 11:22:39 +0000

    Add initial variable definitions and data type examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const account_ID = 144553
+let accountEmail = "tiwaribhaiya@gmail.com"
+var accountPasword = "12345"
+accountCity = "Jaipur"
+
+console.table([account_ID,accountPasword,accountEmail,accountCity])
---
+++ 01_basics/03_Converison.js
@@ -0,0 +1,17 @@
+/* let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let Login = 1
+let Log= Boolean(Login)
+console.log(Log);
+*/
+
+console.log(1+"2");
+console.log("1"+2);
+console.log(1+2+"2");
+
+
