e677286068f472eae618fd97f5590825d7cc8b11
Prathmesh Tiwari <122382508+prathmeshtiwari22@users.noreply.github.com>
2025-09-18 15:46:58 +0000

    "DAY 6"

---
+++ 01_basics/30_call.js
@@ -0,0 +1,16 @@
+// call this
+
+function SetUsername(username){
+    this.username=username
+    console.log("called");
+    
+}
+
+function createUser(username,email,password){
+    SetUsername.call(this,username) // reference call karke rakha hai uska
+    // this.username=username
+    this.email=email
+    this.password=password
+}
+const chai = new createUser("chai","chai@fb.com",'123')
+console.log(chai);
